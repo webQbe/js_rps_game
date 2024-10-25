d06f43e25a33984dea8152cb7ba989732ef7aabb
webQbe <webcubesolutionslk@gmail.com>
2024-10-25 20:32:44 +0530

    Created scoreboard Object to keep score

--- js/main.js
+++ js/main.js
@@ -6,4 +6,10 @@ const result = document.getElementById('result');
 const restart = document.getElementById('restart');
 const modal = document.querySelector('.modal');
 
+// Create scoreboard Object to Keep Score
+const scoreboard = {
+    player: 0,
+    computer: 0
+}
+
 
