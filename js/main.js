39cc2264be4776a494aad1a3b014fad392920512
webQbe <webcubesolutionslk@gmail.com>
2024-10-25 20:30:06 +0530

    Defined DOM elements

---
+++ js/main.js
@@ -0,0 +1,9 @@
+
+// Define DOM Elements
+const choices = document.querySelectorAll('.choice');
+const score = document.getElementById('score');
+const result = document.getElementById('result');
+const restart = document.getElementById('restart');
+const modal = document.querySelector('.modal');
+
+
